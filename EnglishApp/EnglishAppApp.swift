427c747623d8ecefcaaf9026df45d64f8c017e32
ShionNakamura <shionnakamura0120@gmail.com>
2025-06-10 23:23:17 +0900

    Initial Commit

---
+++ EnglishApp/EnglishAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishAppApp.swift
+//  EnglishApp
+//
+//  Created by 仲村士苑 on 2025/06/10.
+//
+
+import SwiftUI
+
+@main
+struct EnglishAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EnglishAppTests/EnglishAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishAppTests.swift
+//  EnglishAppTests
+//
+//  Created by 仲村士苑 on 2025/06/10.
+//
+
+import Testing
+@testable import EnglishApp
+
+struct EnglishAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
